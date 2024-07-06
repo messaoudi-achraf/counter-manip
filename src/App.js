fccc7f9937b31a20e413f0bf335981815ac6976d
messaoudi-achraf <achmess9@gmail.com>
2024-07-06 21:44:38 +0100

    devolopped

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ function App() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+
   return (
     <div>
       <div className="step">
-        <button
+        <input type="range" min="0" max="10" value={step} onChange={(e)=>setStep(Number(e.target.value))} />
+        <span>Step : {step}</span>
+        <br/>
+        {/* <button
           onClick={() => {
             setStep((s) => s - 1);
           }}>
           -
         </button>
         <span>Step : {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <button onClick={() => setStep((s) => s + 1)}>+</button> */}
       </div>
 
       <div className="count">
         <button onClick={() => setCount((s) => s - step)}>-</button>
-        <span>Count : {count}</span>
+        <input type="placeholder" value={count} onChange={(e)=>setCount(Number(e.target.value))}/>
         <button onClick={() => setCount((s) => s + step)}>+</button>
 
         {count === 0 ? (
@@ -38,6 +42,8 @@ function App() {
 
         {/* {count >= 0 ? <p>{count} days from now is {date.toDateString()}</p> : <p>{-count} days ago was {date.toDateString()}</p>} */}
       </div>
+      {(count !== 0 || step !== 1) ?  <button onClick={()=> (setCount(0) ,
+      setStep(1))}>Reset</button> : null }
     </div>
   );
 }
